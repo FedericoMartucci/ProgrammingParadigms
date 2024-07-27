5a60807312c7e24313714e3676d0096e8fcf646c
FedericoMartucci <federicomartucci@hotmail.com>
2024-07-27 17:24:51 -0300

    feat: do exercise 1 from diagnosis guide

--- GuiaDiagnostico/src/ejercicios/Ejercicio01.java
+++ GuiaDiagnostico/src/ejercicios/Ejercicio01.java
@@ -15,7 +15,23 @@ public class Ejercicio01 {
 	 * 7 8 2
 	 */
 	public static boolean resolver(int[][] m) {
-		// completar
-		return false;
+		int numFilas = m.length;
+	    int numColumnas = m[0].length;
+	    int i;
+	    int acum = m[0][0];
+	    
+		if(numFilas != numColumnas) {
+			System.out.println("La matriz no es cuadrada.");
+			return false;
+		}
+		
+	    for(i = 1; i < numFilas; i++) {
+	    	System.out.printf("%d - %d\n",acum, m[i][i]);
+	    	if(m[i][i] != acum)
+	    		return false;
+	    	acum += m[i][i];
+	    }
+		
+		return true;
 	}
 }
--- GuiaDiagnostico/src/main/Main.java
+++ GuiaDiagnostico/src/main/Main.java
@@ -1,9 +1,32 @@
 package main;
 
+import ejercicios.Ejercicio01;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][]m = {
+				{6, 2, 3, 4, 5},
+				{1, 6, 3, 4, 5},
+				{1, 2, 12, 4, 5},
+				{1, 2, 3, 24, 5},
+				{1, 2, 3, 4, 48}
+		};
+		int[][]m2 = {
+				{1, 2, 3},
+				{4, 1, 6},
+				{7, 8, 2},
+		};
+		int[][]m3 = {
+				{1, 2, 3},
+				{4, 2, 6},
+				{7, 8, 2},
+		};
+
+		if(Ejercicio01.resolver(m3))
+			System.out.println("Comprueba la regla");
+		else
+			System.out.println("No comprueba la regla");
 
 	}
 
