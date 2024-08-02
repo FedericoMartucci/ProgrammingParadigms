83f92d6238aced26742f9ebe12a1ed0c9df516a3
FedericoMartucci <federicomartucci@hotmail.com>
2024-08-01 23:10:37 -0300

    fix: add constructors to class Punto.

--- GuíaObjetos/src/objetos/ejercicio02/Punto.java
+++ GuíaObjetos/src/objetos/ejercicio02/Punto.java
@@ -4,6 +4,16 @@ public class Punto {
 	private double x;
 	private double y;
 	
+	public Punto() {
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public Punto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
 	public double obtenerX() {
 		return this.x;
 	}
